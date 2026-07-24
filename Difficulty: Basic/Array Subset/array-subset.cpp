1d525bd007d7bba2420decccfafa99439b965171
Ashutosh Shukla <ashutosh.shukla18503@gmail.com>
2026-07-24 13:45:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubset(vector<int> &a, vector<int> &b) {
+
+        unordered_map<int, int> mp;
+
+        for (int x : a)
+            mp[x]++;
+
+        for (int x : b) {
+            if (mp[x] == 0)
+                return false;
+
+            mp[x]--;
+        }
+
+        return true;
+    }
+};
